2d99a86fa195dcaf2502e0748ad5a5287845fdac
Guiando <Guiando@G036>
2022-05-24 17:47:13 -0300

    Add project files.

---
+++ Renomeando_Arquivos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Renomeando_Arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "=M_e-u Nome é &&Gils)on";
+            string[] charsToRemove = new string[] {"=","-","&",")","_"};//caracteres a serem eliminados
+            foreach (var c in charsToRemove)                             //verificar se existe comando unico 
+            {                                                           
+                nome = nome.Replace(c, string.Empty);
+            }
+            Console.WriteLine(nome);
+        }
+    }
+}
+
+//O foreach é um recurso do C# que possibilita executar um conjunto de comandos para cada elemento presente em uma coleção (Array, List, Stack, Queue e outras).
